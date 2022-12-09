7c82c41d26a359c134118f4948c39739330ddc09
nigarvalieva <vvaliyevaa@gmail.com>
2022-12-09 20:32:24 +0400

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function onClick(e) {
+    if (e.target.className==="minus" && Number(document.querySelector(".num").textContent)>-10){
+      document.querySelector(".num").textContent = Number(document.querySelector(".num").textContent)-1
+    } else if (e.target.className==="plus" && Number(document.querySelector(".num").textContent)<10){
+      document.querySelector(".num").textContent = Number(document.querySelector(".num").textContent)+1
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="minus" onClick={onClick}>-</button>
+      <p className="num">0</p>
+      <button className="plus" onClick={onClick}>+</button>
     </div>
   );
 }
